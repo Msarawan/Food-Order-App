d4a9d1489e2026cc5c279145cc9b347b9be4fe10
Msarawan <msarawan@redhat.com>
2022-09-21 23:10:33 +0530

    added test folder in the component

---
+++ src/components/Meals/mealsSummary/test/MealSummary.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MealSummary from "../../mealsSummary/MealsSummary"
+import { mount } from "enzyme";
+
+describe("Food Order aap testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<MealSummary/>);
+  });
+
+  test("render the title of meal summary", () => {
+    expect(wrapper.find("h2").text()).toContain("Delicious Food, Delivered To You");
+  });
+
+  test("render the discription of meal", () => {
+    expect(wrapper.find(".summary").text()).toContain("Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.");
+  });
+
+  test("render the discription", () => {
+    expect(wrapper.find(".summary").text()).toContain("All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!")});
+
+});
