3e549cf6360d2d47e73a672ff99591fc98aa67f6
Msarawan <msarawan@redhat.com>
2022-09-27 01:37:25 +0530

    Modified the test cases

--- src/components/Meals/mealItemForm/test/MealItemForm.test.js
+++ src/components/Meals/mealItemForm/test/MealItemForm.test.js
@@ -1,6 +1,7 @@
 import MealItemForm from "../../mealItemForm/MealItemForm"
 import React from "react";
-import { mount } from "enzyme";
+import {mount,shallow} from "enzyme";
+
 
 describe("Food Order app testing", () => {
   let wrapper;
@@ -13,13 +14,22 @@ describe("Food Order app testing", () => {
 
    test("add the quantity by clicking add button", () => {
     expect(wrapper.props().onClick).toEqual('{addToCartHandler}');
-   // console.log(wrapper.find('MealItemForm'))
+      console.log(wrapper.find('MealItemForm'))
    });
 
   test("render sumbit handler", () => {
     expect(wrapper.props().onSubmit).toEqual('{submitHandler}');
   });
 
+ const clickFn = jest.fn();
+  test('button click should hide component', () => {
+    const wrapper = shallow(<MealItemForm onClick={clickFn} />);
+    wrapper
+      // .find('props.id')
+      .simulate('click');
+    // expect(clickFn).toHaveBeenCalled();
+    expect(clickFn).toMatchSnapshot();
+  });
 
   test('renders a snapshot', () => {
     expect(wrapper).toMatchSnapshot();
--- src/components/UI/modal/test/Modal.test.js
+++ src/components/UI/modal/test/Modal.test.js
@@ -1,6 +1,31 @@
 import React from "react";
- import Modal from "../../../UI/modal/Modal"
+ import Modal from "../../modal/Modal"
  import { mount } from "enzyme";
+
+//  import {
+//   // Backdrop,
+//   ModalOverlay,
+// } from '../../modal/Modal';
+ 
+
+//  describe('Modal component', () => {
+//   let component;
+
+//    const modalRoot = global.document.createElement('div');
+//     modalRoot.setAttribute('id', 'overlays');
+//     const body = global.document.querySelector('body');
+//     body.appendChild(modalRoot);
+
+//     // afterEach(() => {
+//     //   component.unmount();
+//     // });
+
+//     test('should render the modal component', () => {
+//       component = mount(
+//         <Modal/>
+//       );
+//       expect(modalRoot.hasChildNodes()).toBeTruthy();
+
 
  describe("Food Order app testing", () => {
   let wrapper;
@@ -15,5 +40,4 @@ import React from "react";
   test('renders a snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
-
  })
