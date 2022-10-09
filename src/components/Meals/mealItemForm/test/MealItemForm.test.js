1e859bc3549c51b0016acce7e3c7daf410f702e3
Msarawan <msarawan@redhat.com>
2022-10-10 00:28:53 +0530

    Writing the test cases and generating coverage report

--- src/components/Meals/mealItemForm/test/MealItemForm.test.js
+++ src/components/Meals/mealItemForm/test/MealItemForm.test.js
@@ -1,16 +1,21 @@
 //import 'jsdom-global/register';
-import MealItemForm from "../../mealItemForm/MealItemForm"
+import MealItemForm from "../../mealItemForm/MealItemForm";
 import React from "react";
-import {mount} from "enzyme";
+import {mount,shallow} from "enzyme";
 
 
 jest.mock('../../../UI/input/Input');
 
 describe("Food Order app testing", () => {
+  
   let wrapper;
   beforeEach(() => {
-    wrapper = mount(<MealItemForm onSubmit='{submitHandler}' onClick='{addToCartHandler}'/>);
+    wrapper = mount(<MealItemForm onSubmit='{submitHandler}' onClick='{addToCartHandler}' />);
   });
+
+  test("renders correctly", () =>{
+    wrapper=mount(<MealItemForm/>)
+  })
 
   test("render the Add button", () => {
      expect(wrapper.find('button').text()).toContain('+ Add')})
@@ -22,9 +27,38 @@ describe("Food Order app testing", () => {
 
   test("render sumbit handler", () => {
     expect(wrapper.props().onSubmit).toEqual('{submitHandler}');
-   
-  });
-  
-  expect(wrapper).toMatchSnapshot();
+  })
+
+ 
+  test("render event preventDefault",() =>{
+  const event = { preventDefault: jest.fn() };
+  expect(event.preventDefault).toMatchSnapshot();
+  })
+  expect(wrapper).toMatchSnapshot();  
+
+
+//   const setAmountIsValid = jest.fn();
+//   const useStateSpy = jest.spyOn(React, 'useState');
+//   useStateSpy.mockImplementation(init => [init, setAmountIsValid]);
+
+//  expect(setAmountIsValid).toMatchSnapshot();
+//   test('should update state on input change', () => {
+//     const newInputValue = false;
+//     wrapper.find('submitHandler').simulate('change', { target: { value: newInputValue } });
+//     expect(setAmountIsValid).toHaveBeenCalledWith(newInputValue);
+// });
+
+  test('does not reload page after submition', () => { 
+    const wrapper = shallow(<MealItemForm />)
+    const event = { preventDefault:() => {} }
+    jest.spyOn(event, 'preventDefault')
+    wrapper.find('form').simulate('submit', event)
+    expect(event.preventDefault).toBeCalled()
+  })
+
 
 })
+
+
+
+
