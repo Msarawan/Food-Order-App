968831d37e68f4865ce0de06fb30157373399e9e
Msarawan <msarawan@redhat.com>
2022-11-08 18:42:38 +0530

    Added webpack to Food-Order-App

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module:{
+    rules:[
+      {
+      test: /\.tsx?$/,
+      exclude: [/node_modules/],
+      use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react'],
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png)$/i,
+        type: "asset",
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:"./public/index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+
+};
